129c55b288e965b93fc02c82fbd4e52c380fa419
Egor <yatskoegor@gmail.com>
2021-05-14 13:00:08 +0300

    FilmInfo comments added

--- FilmHub/wwwroot/js/site.js
+++ FilmHub/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const tx = document.getElementsByTagName("textarea");
+for (let i = 0; i < tx.length; i++) {
+    tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+    tx[i].addEventListener("input", OnInput, false);
+}
+
+function OnInput() {
+    this.style.height = "auto";
+    this.style.height = (this.scrollHeight) + "px";
+}
